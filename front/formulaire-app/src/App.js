61acc2e50e008f4b35bc49a243118e6e2aaf4e98
Arlensiu Celis <arlensiu.celis@gmail.com>
2019-01-30 11:23:01 +0100

    Update formumaire's style.

--- front/formulaire-app/src/App.js
+++ front/formulaire-app/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import "./App.css";
-import SignUp from "./components/SignUp";
+import React, { Component } from 'react';
+import './App.css';
+import SignUp from './components/SignUp';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div align='center'>
         <SignUp />
       </div>
     );
--- front/formulaire-app/src/components/SignUp.jsx
+++ front/formulaire-app/src/components/SignUp.jsx
@@ -1,107 +1,119 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-      email: "",
-      password: "",
-      passwordVerification: "",
-      name: "",
-      lastName: ""
+      email: '',
+      password: '',
+      passwordVerification: '',
+      name: '',
+      lastName: '',
+      alert: 'none'
     };
-    this.updateEmailField = this.updateEmailField.bind(this);
-    this.updatePasswordField = this.updatePasswordField.bind(this);
-    this.updatePasswordVerificationField = this.updatePasswordVerificationField.bind(
-      this
-    );
-    this.updateNameField = this.updateNameField.bind(this);
-    this.updateLastNameField = this.updateLastNameField.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  updateEmailField(event) {
+  updateEmailField = event => {
     this.setState({ email: event.target.value });
-  }
+  };
 
-  updatePasswordField(event) {
+  updatePasswordField = event => {
     this.setState({ password: event.target.value });
-  }
+  };
 
-  updatePasswordVerificationField(event) {
+  updatePasswordVerificationField = event => {
     this.setState({ passwordVerification: event.target.value });
-  }
+  };
 
-  updateNameField(event) {
+  updateNameField = event => {
     this.setState({ name: event.target.value });
-  }
+  };
 
-  updateLastNameField(event) {
+  updateLastNameField = event => {
     this.setState({ lastName: event.target.value });
-  }
+  };
 
-  handleSubmit(event) {
-    console.log(JSON.stringify(this.state));
+  handleSubmit = event => {
     event.preventDefault();
     this.setState({
-      email: "",
-      password: "",
-      passwordVerification: "",
-      name: "",
-      lastName: ""
+      email: '',
+      password: '',
+      passwordVerification: '',
+      name: '',
+      lastName: '',
+      alert: 'block'
     });
-    alert("Your information has been submitted");
-  }
+    setTimeout(() => {
+      this.setState({ alert: 'none' });
+    }, 1500);
+  };
 
   render() {
+    let entries = Object.entries(this.state);
     return (
-      <div className="display">
-        <h3 className="idInfo">
-          Verify your information: {JSON.stringify(this.state)}
-        </h3>
-        <form className="content" onSubmit={this.handleSubmit}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-8 offset-2'>
+            <h5 className='my-3'>Verify your information:</h5>
+            {entries.map(state => {
+              return (
+                <p key={state[0]}>
+                  {state[0]}: {state[1]}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+        <form className='content' onSubmit={this.handleSubmit}>
           <input
-            type="email"
-            name="email"
-            placeholder="Your email"
+            type='email'
+            name='email'
+            placeholder='Your email'
             value={this.state.email}
             onChange={this.updateEmailField}
           />
           <br />
           <input
-            type="password"
-            name="password"
-            placeholder="Your password"
+            type='password'
+            name='password'
+            placeholder='Your password'
             value={this.state.password}
             onChange={this.updatePasswordField}
           />
           <br />
           <input
-            type="password"
-            name="passwordVerification"
-            placeholder="Please re-enter your password"
+            type='password'
+            name='passwordVerification'
+            placeholder='Please re-enter your password'
             value={this.state.passwordVerification}
             onChange={this.updatePasswordVerificationField}
           />
           <br />
           <input
-            type="text"
-            name="name"
-            placeholder="Your name here"
+            type='text'
+            name='name'
+            placeholder='Your name here'
             value={this.state.name}
             onChange={this.updateNameField}
           />
           <br />
           <input
-            type="text"
-            name="last_name"
-            placeholder="Your last name here"
+            type='text'
+            name='last_name'
+            placeholder='Your last name here'
             value={this.state.lastName}
             onChange={this.updateLastNameField}
           />
           <br />
-          <input type="submit" value="Submit" />
+          <input type='submit' value='Submit' />
         </form>
+        <div
+          className='alert alert-primary col-12'
+          role='alert'
+          style={{ display: `${this.state.alert}` }}
+        >
+          Form has been sent
+        </div>
       </div>
     );
   }
